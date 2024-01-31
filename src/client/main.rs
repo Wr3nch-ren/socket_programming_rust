c9535b0309ccd6f953d2ea2e51fe27ab42759ae8
Wr3nch_ren <76581683+Wr3nch-ren@users.noreply.github.com>
2024-01-31 19:46:48 +0700

    Add Comments for client rust

--- src/client/main.rs
+++ src/client/main.rs
@@ -5,8 +5,10 @@ use std::{
 fn main() {
     println!("Starting client...");
     let mut tcp_stream = TcpStream::connect("127.0.0.1:7878").unwrap();
-    //tcp_stream.write_all(b"GET / HTTP/1.1\r\n").unwrap();
-    tcp_stream.write_all(b"SHAKEHAND /foo HandShaker/0.1\r\n").unwrap();
+    // Successful Request
+    tcp_stream.write_all(b"SHAKEHAND / HandShaker/0.1\r\n").unwrap();
+    // Unsuccessful Request
+    // tcp_stream.write_all(b"SHAKEHAND /foo HandShaker/0.1\r\n").unwrap();
     tcp_stream.flush().unwrap();
     let mut buffer = [0; 1024];
     let _size = tcp_stream.read(&mut buffer).unwrap();
